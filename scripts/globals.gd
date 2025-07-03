25d847197535909ecd783941b8069357c8852b31
unknown <xenordoz@gmail.com>
2025-07-03 05:54:28 +0200

    Generating map with 2 tile colors instead of one, but bug when up and left

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -3,6 +3,7 @@ extends Node
 # Forced parameters
 var chunkSize = 16 # Number of tiles in a chunk
 var tileSize = 16 # Size of a tile
+var totalTileNumbre = 2 # Number of textures available for tile generation
 
 # Player-toggled parameters
-var loadedChunkDistance = 5 # Number of chunks created around the player (in a square)
+var loadedChunkDistance = 1 # Number of chunks created around the player (in a square)
--- scripts/layerHolder.gd
+++ scripts/layerHolder.gd
@@ -11,6 +11,7 @@ const STONE = preload("res://assets/tiles/stone.png")
 # Var used later
 var chunkPos: Vector2i
 var time = 0.0
+var tileID = 0 # Default tile ID will be grass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -54,7 +55,59 @@ func generateWorld(from: Vector2i, to: Vector2i) -> void:
 		for chunkX in range (from.x-1, to.x+1, 1):
 			if background.get_cell_source_id(Vector2i(chunkX * Globals.chunkSize, chunkY * Globals.chunkSize)) == -1:
 				print("\t\tChunk ", Vector2i(chunkX, chunkY), " detected as unloaded !")
+				
 				for y in range(chunkY * Globals.chunkSize, (chunkY+1) * Globals.chunkSize, 1):
 					for x in range(chunkX * Globals.chunkSize, (chunkX+1) * Globals.chunkSize, 1):
-						background.set_cell(Vector2i(x, y), 0,Vector2i(randi_range(0,1),randi_range(0,1)))
+						
+						# Choosing what type of tile do we want
+						var neighborsID = getNeighbors(x, y)
+						var possibleIds = []
+						if not neighborsID.is_empty() :
+							for id in neighborsID.keys():
+								for i in range(neighborsID[id]):
+									possibleIds.append(id)
+							if neighborsID.keys().size() == 1 and neighborsID[neighborsID.keys()[0]] >= 4:
+								for i in range(Globals.totalTileNumbre): # Add every ID possible to diversify
+									possibleIds.append(i)
+							tileID = possibleIds[randi() % possibleIds.size()]
+						
+						background.set_cell(Vector2i(x, y), tileID,Vector2i(randi_range(0,1),randi_range(0,1)))
 						#set_cell(<cell position> Vector2i, <Tilse set id> Int, <position on atlas> Vector2i)
+			
+			# We check again cells to eliminate the ones that are too small
+				for y in range(chunkY * Globals.chunkSize, (chunkY+1) * Globals.chunkSize, 1):
+					for x in range(chunkX * Globals.chunkSize, (chunkX+1) * Globals.chunkSize, 1):
+						var neighborsID = getNeighbors(x, y)
+						
+						# Checking if we have the dictionnary
+						if not neighborsID.is_empty() :
+							# Grabbing ID of the cell and check if neighbors have it too
+							var currentID = background.get_cell_source_id((Vector2i(x,y)))
+							var currentIDCount = 0
+							if neighborsID.has(currentID):
+								currentIDCount = neighborsID[currentID]
+								
+							# If less than 2 neighbors have this ID, then we give the same one as most of them
+							if currentIDCount < 7:
+								var maxCount = 0
+								var majorID = currentID
+								for id in neighborsID.keys():
+									if neighborsID[id] > maxCount:
+										maxCount = neighborsID[id]
+										majorID = id
+									background.set_cell(Vector2i(x,y), majorID, Vector2i(randi_range(0,1),randi_range(0,1)))
+
+
+func getNeighbors(x: int, y: int) -> Dictionary :
+	# Choosing what type of tile do we want
+	var neighborsID = {}
+	for neighborsY in range(y-1, y+2, 1):
+		for neighborsX in range(x-1, x+2, 1):
+			if neighborsY != y or neighborsX != x :
+				var cellID = background.get_cell_source_id(Vector2i(neighborsX,neighborsY))
+				if cellID != -1 :
+					if neighborsID.has(cellID): # If ID has been seen we add 1 to it
+						neighborsID[cellID] += 1
+					else :  # Otherwise we create it
+						neighborsID[cellID] = 1
+	return neighborsID
